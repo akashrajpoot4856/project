724c497ba2327cd3ebc8837f3d8090505dfb7017
Akash Kumar <kumarakash485606@gmail.com>
2026-08-12 07:18:59 +0530

    Implement password generator in passward.py
    Added a password generator that creates a random password based on user-defined length.

---
+++ passward.py
@@ -0,0 +1,15 @@
+# project
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + "!@#$%^&*"
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
+
